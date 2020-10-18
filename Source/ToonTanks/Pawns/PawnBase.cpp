f582709de7c3f1d82f3ca9d8da9c391c71a6b057
Is0top <yurakh@gmail.com>
2020-10-18 23:42:12 +0300

    Added a PawnTank class inherited from PawnBase; Added a Spring Arm and Camera components to PawnTank class;

--- Source/ToonTanks/Pawns/PawnBase.cpp
+++ Source/ToonTanks/Pawns/PawnBase.cpp
@@ -4,10 +4,9 @@
 #include "PawnBase.h"
 #include "Components/CapsuleComponent.h"
 
-// Sets default values
 APawnBase::APawnBase()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
@@ -18,7 +17,7 @@ APawnBase::APawnBase()
 	{
 		BaseMesh->SetupAttachment(RootComponent);
 	}
-	
+
 	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Turret Mesh"));
 	if (TurretMesh && BaseMesh)
 	{
@@ -31,25 +30,3 @@ APawnBase::APawnBase()
 		ProjectileSpawnPoint->SetupAttachment(TurretMesh);
 	}
 }
-
-// Called when the game starts or when spawned
-void APawnBase::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void APawnBase::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-// Called to bind functionality to input
-void APawnBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
-
