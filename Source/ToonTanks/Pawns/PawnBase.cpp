acdc6eeea847a30d544f2b9d48536dcee280b745
Is0top <yurakh@gmail.com>
2020-11-06 20:39:35 +0300

    Prepared PawnBase class for functionality extension

--- Source/ToonTanks/Pawns/PawnBase.cpp
+++ Source/ToonTanks/Pawns/PawnBase.cpp
@@ -30,3 +30,25 @@ APawnBase::APawnBase()
 		ProjectileSpawnPoint->SetupAttachment(TurretMesh);
 	}
 }
+
+void APawnBase::RotateTurretFunction(FVector LookAtTarget)
+{
+	// Update TurretMest rotation to face towards LookAtTarget provided from child classes
+	// TurretMesh->SetWorldRotation()...
+}
+
+void APawnBase::Fire()
+{
+	// Get ProjectileSpawnPoint Location && Rotation -> Spawn Projectile at Location firing towards Rotation
+}
+
+void APawnBase::HandleDestruction()
+{
+	// --- Universal Functionality ---
+	// Play death effects particle, sound and camera shake
+
+	// --- Then do Child overrides ---
+	// -- PawnTurret - Inform Game mode Turret died, then Destroy() self
+
+	// -- PawnTank - Inform Game mode Player died, then Hide() all components and stop movement input
+}
