50886179d0a7ad5ea92571d09c4e28d94148e2f0
Is0top <yurakh@gmail.com>
2020-11-15 00:21:41 +0300

    Created and utilized TankGameModeBase and its blueprint

---
+++ Source/ToonTanks/GameModes/TankGameModeBase.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankGameModeBase.h"
+
+void ATankGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	// get references and game win/lose condition
+	//HandleGameStart();
+}
+
+void ATankGameModeBase::ActorDied(AActor* DeadActor)
+{
+	// handle death of a turret
+	// or a player
+}
+
+void ATankGameModeBase::HandleGameStart()
+{
+	//initialize player, turrets, score, countdown
+	//call Blueprint GameStart();
+}
+
+void ATankGameModeBase::HandleGameOver(bool PlayerWon)
+{
+	// Check if player won (destroyed all turrets within given time) or lost (died or ran out of time)
+	// display gameover message
+	// call Blueprint version of GameOver();
+}
